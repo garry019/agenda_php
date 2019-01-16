5179efcb26c4eb769c3faf64979776444a9ad609
Gabriel Calderon <garry019@gmail.com>
2019-01-16 12:17:14 -0500

    Selectores de Fecha y Hota corregidos

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,3 @@
-$(document).ready(function() {
-
-});
 
 class EventsManager {
     constructor() {
@@ -212,20 +209,20 @@ $(function(){
 
 function initForm(){
   $('#start_date, #titulo, #end_date').val('');
-  // $('#start_date, #end_date').datepicker({
-  //   dateFormat: "yy-mm-dd"
-  // });
-  // $('.timepicker').timepicker({
-  //   timeFormat: 'HH:mm',
-  //   interval: 30,
-  //   minTime: '5',
-  //   maxTime: '23:30',
-  //   defaultTime: '7',
-  //   startTime: '5:00',
-  //   dynamic: false,
-  //   dropdown: true,
-  //   scrollbar: true
-  // });
+  $('#start_date, #end_date').datepicker({
+    dateFormat: "yy-mm-dd"
+  });
+  $('.timepicker').timepicker({
+    timeFormat: 'HH:mm',
+    interval: 30,
+    minTime: '5',
+    maxTime: '23:30',
+    defaultTime: '7',
+    startTime: '5:00',
+    dynamic: false,
+    dropdown: true,
+    scrollbar: true
+  });
   $('#allDay').on('change', function(){
     if (this.checked) {
       $('.timepicker, #end_date').attr("disabled", "disabled")
